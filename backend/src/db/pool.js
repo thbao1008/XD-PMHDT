658cc5582ed1dc94a925aa7409a6291bed7d6139
ntbao1008 <ntb100804@gmail.com>
2025-11-06 01:54:52 +0700

    đằng tải các gói học từ admin

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -5,12 +5,6 @@ dotenv.config();
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  // hoặc:
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASS,
-  // port: Number(process.env.DB_PORT || 5432),
 });
 
 export default pool;
